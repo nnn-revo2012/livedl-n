12b1959514805443e5bf76c5060098985cd983b8
nnn-revo2012 <nnn_revo2012@yahoo.co.jp>
2021-02-26 11:38:04 +0900

    Update build.ps1 etc.

---
+++ src/build-386.ps1
@@ -0,0 +1,7 @@
+
+go clean -cache
+set-item env:GO111MODULE -value on
+set-item env:GOARCH -value 386
+set-item env:CGO_ENABLED -value 1
+rm ..\livedl.x86.exe
+go build -o ..\livedl.x86.exe .\livedl.go
